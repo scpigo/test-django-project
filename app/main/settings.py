64b57708c4865a8d80a73dbdc2fade0e21ea955a
sc_pigo <maximiliyan99@mail.ru>
2025-08-15 11:09:57 +0300

    обновил Dockerfile

--- app/main/settings.py
+++ app/main/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 from pathlib import Path
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
